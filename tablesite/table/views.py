bfaaa3d95f6fd42828a0d7d93d595b0a3bf4496d
tamara <tamara@ubuntu20.04>
2022-04-23 17:15:37 +0300

    final commit for test project

--- tablesite/table/views.py
+++ tablesite/table/views.py
@@ -6,8 +6,5 @@ def home(request):
     return render(request, "table/home.html", {})
 
 def table(request):
-    
-    table_content = TableContent.objects.values()
-    print(table_content)
+    return render(request, "table/table.html", {})
 
-    return render(request, "table/table.html", {"table content": table_content})
